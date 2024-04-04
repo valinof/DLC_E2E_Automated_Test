b79d737dbad1aa064343aaa9b47f52174be7f7ef
Finbar Valino <valinof@gmail.com>
2024-04-04 13:39:06 -0500

    Create new page object class for Customer Information page

---
+++ cypress/support/PageObject/DLC_WP_Customer_Information_Page.js
@@ -0,0 +1,25 @@
+import TestUtil from "../Test_Utilities/Test_Utilities";
+
+const testUtil = new TestUtil();
+
+class CustomerInfoEntryPage {
+  elements = {
+    weNeedInfoTitle: () =>
+      cy.get("[data-js-reload='field_4_19'] .loan-apply-account-title"),
+    custInfoFirstNameField: () => cy.get("#input_4_13"),
+    custInfoLastNameField: () => cy.get("#input_4_15"),
+    dobField: () => cy.get("#input_4_17"),
+    ssnField: () => cy.get("#input_4_18"),
+  };
+  checkCustomerInfoPageTitle() {
+    this.elements
+      .weNeedInfoTitle()
+      .should("contain", "we’ll need a little info");
+  }
+
+  enterTestFirstName(testFirstName) {
+    this.elements.custInfoFirstNameField().type("Test" + testFirstName);
+  }
+}
+
+module.exports = CustomerInfoEntryPage;
